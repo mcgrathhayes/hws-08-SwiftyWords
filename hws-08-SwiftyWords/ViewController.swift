22153b1696a752cb369831c3982b8724a46ef7da
Philip Hayes <philip@mcgrathhayes.com>
2020-06-25 11:51:48 -0500

    added labels and buttons to complete UI

--- hws-08-SwiftyWords/ViewController.swift
+++ hws-08-SwiftyWords/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
     
     var cluesLabel: UILabel!
     var answersLabel: UILabel!
-    var currentLabel: UILabel!
+    var currentAnswer: UITextField!
     var scoreLabel: UILabel!
-    var letterButtons: UILabel!
+    var letterButtons = [UIButton]()
     
     override func loadView() {
         // Create empty main view with parent class of all UIKit view types: labels, buttons, progress views, etc
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
         cluesLabel.text = "CLUES"
         cluesLabel.numberOfLines = 0
         view.addSubview(cluesLabel)
+        cluesLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         
         // Create "answers label"
         answersLabel = UILabel()
@@ -44,8 +45,33 @@ class ViewController: UIViewController {
         answersLabel.numberOfLines = 0
         answersLabel.textAlignment = .right
         view.addSubview(answersLabel)
+        answersLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         
+        // Create "current answer" text field
+        currentAnswer = UITextField()
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+        currentAnswer.placeholder = "Tap letters to guess"
+        currentAnswer.textAlignment = .center
+        currentAnswer.font = UIFont.systemFont(ofSize: 44)
+        currentAnswer.isUserInteractionEnabled = false
+        view.addSubview(currentAnswer)
         
+        // Create "submit" button
+        let submit = UIButton(type: .system)
+        submit.translatesAutoresizingMaskIntoConstraints = false
+        submit.setTitle("SUBMIT", for: .normal)
+        view.addSubview(submit)
+        
+        // Create "clear" button
+        let clear = UIButton(type: .system)
+        clear.translatesAutoresizingMaskIntoConstraints = false
+        clear.setTitle("CLEAR", for: .normal)
+        view.addSubview(clear)
+        
+        // Create a container for letter buttons
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
         
         // Activate array of multiple constraints simultaneously
         NSLayoutConstraint.activate([
@@ -71,11 +97,59 @@ class ViewController: UIViewController {
             answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
             
             // Make the answers label match the height of the clues label
-            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor)
+            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+            
+            // Center the current answer text field below the clues label
+            currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20),
+            
+            // Position the current answer and clear buttons
+            submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+            submit.heightAnchor.constraint(equalToConstant: 44),
+            
+            clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
+            clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+            clear.heightAnchor.constraint(equalToConstant: 44),
+            
+            // Position the letter buttons container
+            buttonsView.widthAnchor.constraint(equalToConstant: 750),
+            buttonsView.heightAnchor.constraint(equalToConstant: 320),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor, constant: 20),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20),
         ])
         
-        cluesLabel.backgroundColor = .gray
-        answersLabel.backgroundColor = .lightGray
+        // Set width and height for letter buttons
+        let width = 150
+        let height = 80
+        
+        // Create 20 buttons in a 4x5 grid
+        for row in 0..<4 {
+            for col in 0..<5 {
+                // Create a button and set the font size
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                
+                // Display temporary text in button to make it visible
+                letterButton.setTitle("WWW", for: .normal)
+                
+                // Calculate the button frame based on row and column
+                let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                // Add button to buttons view
+                buttonsView.addSubview(letterButton)
+                
+                // Add button to letterButtons array
+                letterButtons.append(letterButton)
+            }
+        }
+        
+//        cluesLabel.backgroundColor = .gray
+//        answersLabel.backgroundColor = .lightGray
+//        buttonsView.backgroundColor = .yellow
     }
 
     override func viewDidLoad() {
