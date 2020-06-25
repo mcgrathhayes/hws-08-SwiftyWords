955ec8f49b2cb549569c14fe0f1ab847b182105c
Philip Hayes <philip@mcgrathhayes.com>
2020-06-25 07:23:03 -0500

    initial xcode project creation

---
+++ hws-08-SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hws-08-SwiftyWords
+//
+//  Created by Philip Hayes on 6/25/20.
+//  Copyright © 2020 PhilipHayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
